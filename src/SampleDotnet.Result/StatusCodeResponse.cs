82901e69b0391449bb4d0b21fb2c64a9145ea9dc
Mustafa Salih ASLIM <salih@aslim.net>
2023-02-25 20:48:37 +0300

    custom status code response model is defined

---
+++ src/SampleDotnet.Result/StatusCodeResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace SampleDotnet.Result
+{
+    public class StatusCodeResponse
+        : BaseJsonResult
+    {
+        public StatusCodeResponse(int statusCode) : base(statusCode)
+        {
+        }
+
+        public StatusCodeResponse(int statusCode, IEnumerable<string> errorMessages) : base(statusCode, errorMessages)
+        {
+        }
+
+        public StatusCodeResponse(int statusCode, string errorMessages) : base(statusCode, errorMessages)
+        {
+        }
+
+        public StatusCodeResponse(int statusCode, object body) : base(statusCode, body)
+        {
+        }
+
+        public StatusCodeResponse(int statusCode, IEnumerable<object> body) : base(statusCode, body)
+        {
+        }
+    }
+}
